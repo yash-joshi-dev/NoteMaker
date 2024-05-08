73eae8fb22893c1474f7b03d830f74cd2bc2a6c6
Yash Joshi <y6joshi@uwaterloo.ca>
2024-05-07 20:57:13 -0400

    initial commit; finished backend, frontend login and register

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,6 @@
+import Form from "../components/Form";
+
+function Login() {
+  return <Form route="/api/token/" method="login"></Form>;
+}
+export default Login;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,5 @@
+import Form from "../components/Form";
+function Register() {
+  return <Form route="/api/user/register/" method="register"></Form>;;
+}
+export default Register;
